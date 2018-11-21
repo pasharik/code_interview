0bed7a8de1ff47750e763429705cb0c5f2a29ca2
Pavel Pozdeev <pasharik@mail.ru>
2018-11-21 17:23:17 +0000

    Task 1.1 - unique characters in string

--- src/ru/pasharik/question_1_1/UniqueChar.java
+++ src/ru/pasharik/question_1_1/UniqueChar.java
@@ -5,19 +5,24 @@ import java.util.Map;
 
 /**
  * Created by pasharik on 21/11/18.
+ *
+ * Check if all characters in String are unique:
+ * Have a Int array, each element correspond to each symbol of alphabet
  */
 public class UniqueChar {
     public boolean allUnique(String str) {
-        Map<Character, Integer> map = new HashMap();
+        //Assuming we have only 256 symbols in alphabet.
+        if (str.length() > 256) return false; // If string is longer, it will have duplicates
+        Integer[] intArr = new Integer[256];
         char[] chars = str.toCharArray();
         for (char ch : chars) {
-            if (map.containsKey(ch)) return false;
-            map.put(ch, 1);
+            if (intArr[ch] != null) return false;
+            intArr[ch] = 1;
         }
         return true;
     }
 
     public static void main(String[] args) {
-        System.out.println(new UniqueChar().allUnique("abcd123"));
+        System.out.println(new UniqueChar().allUnique("abcd123a"));
     }
 }
