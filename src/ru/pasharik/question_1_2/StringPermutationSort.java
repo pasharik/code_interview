723b672012f77bf3b02eeb361a3252b30ecb2be0
Pavel Pozdeev <pasharik@mail.ru>
2018-11-27 16:33:09 +0000

    Task 1.2 - one strng is permutation of another - method 1: sort

---
+++ src/ru/pasharik/question_1_2/StringPermutationSort.java
@@ -0,0 +1,27 @@
+package ru.pasharik.question_1_2;
+
+import java.util.Arrays;
+
+/**
+ * Created by pasharik on 27/11/18.
+ *
+ * Check if one string is permutation of another
+ * Method 1: sort both strings (log N), and then check if each symbol is the same
+ */
+public class StringPermutationSort {
+    public boolean isPermutation(String str1, String str2) {
+        if (str1.length() != str2.length()) return false;
+
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        return Arrays.equals(arr1, arr2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new StringPermutationSort().isPermutation(
+                "321daCF",
+                "123daFC"));
+    }
+}
