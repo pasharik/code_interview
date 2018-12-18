97ce093b4cb8458f6c40643bf5d4ada762fc7ef8
Pavel Pozdeev <pasharik@mail.ru>
2018-12-18 17:19:01 +0000

    Task 1.4 - polindrome

---
+++ src/ru/pasharik/question_1_4/Polindrome1.java
@@ -0,0 +1,27 @@
+package ru.pasharik.question_1_4;
+
+/**
+ * Created by pasharik on 18/12/18.
+ */
+public class Polindrome1 {
+    public static boolean isPolindrome(String str) {
+        int[] a = new int[128];
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != ' ') a[str.charAt(i)]++;
+        }
+        boolean hasOdd = false;
+        for (int i = 0; i < 128; i++) {
+            if (a[i] % 2 == 1) {
+                if (hasOdd) {
+                    return false;
+                }
+                hasOdd = true;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPolindrome("taco ca  t"));
+    }
+}
