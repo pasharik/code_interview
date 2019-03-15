1da178b6b52637d507a637f6051a6d32def8be1b
Pavel Pozdeev <pasharik@mail.ru>
2019-03-15 16:40:28 +0000

    Task 1.6 - Compress string

---
+++ src/ru/pasharik/question_1_6/Compress.java
@@ -0,0 +1,31 @@
+package ru.pasharik.question_1_6;
+
+public class Compress {
+    private static String doCompress(String str) {
+        System.out.println(str);
+        StringBuilder sb = new StringBuilder();
+        char oldChar = str.charAt(0);
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (oldChar != ch) {
+                sb.append(oldChar);
+                sb.append(count);
+                if (sb.length() > str.length()) return str;
+                count = 0;
+            }
+            count++;
+            oldChar = ch;
+        }
+        sb.append(oldChar);
+        sb.append(count);
+
+        return sb.length() > str.length() ? str : sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(
+                doCompress("aaaabbbcc"));
+//                doCompress("abcdefgh"));
+    }
+}
