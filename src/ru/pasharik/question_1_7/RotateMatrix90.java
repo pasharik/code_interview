78f102e07b1986eae0ffeb4266a2c025e4d1d52f
Pavel Pozdeev <pasharik@mail.ru>
2019-03-18 18:56:59 +0000

    Task 1.7 - Rotate matrix 90 degrees

---
+++ src/ru/pasharik/question_1_7/RotateMatrix90.java
@@ -0,0 +1,45 @@
+package ru.pasharik.question_1_7;
+
+public class RotateMatrix90 {
+    private static int[][] rotate90(int[][] a) {
+        int n = a.length - 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                //Top quarter: join of two triangles
+                if ((i <= j) //Top-right triangle
+                        && ((n - j) > i)) { //Top-left triangle
+                    int buf = a[i][j];
+                    a[i][j] = a[n - j][i];          //Left -> Top
+                    a[n - j][i] = a[n - i][n - j];  //Bottom -> Left
+                    a[n - i][n - j] = a[j][n - i];  //Right -> Bottom
+                    a[j][n - i] = buf;              //Top -> Right
+                }
+            }
+        }
+
+        return a;
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = new int[][] {
+                {1,0,1,1,1,1},
+                {1,0,1,1,1,1},
+                {1,0,1,1,1,0},
+                {1,0,1,1,1,1},
+                {1,0,1,1,1,1},
+                {1,0,1,1,1,1}
+        };
+        printMatrix(arr);
+        System.out.println("Rotating...");
+        printMatrix(rotate90(arr));
+    }
+
+    private static void printMatrix(int[][] matr) {
+        for (int i = 0; i < matr.length; i++) {
+            for (int j = 0; j < matr[i].length; j++) {
+                System.out.print(matr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
