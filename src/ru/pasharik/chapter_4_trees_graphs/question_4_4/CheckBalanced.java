6094ea6c20417887a4a7ee737ce46ec0b55807d7
Pavel Pozdeev <pasharik@mail.ru>
2019-05-11 16:53:40 +0100

    Task 4.4 - Check if binary tree is balanced

--- src/ru/pasharik/chapter_4_trees_graphs/question_4_4/CheckBalanced.java
+++ src/ru/pasharik/chapter_4_trees_graphs/question_4_4/CheckBalanced.java
@@ -4,26 +4,32 @@ import ru.pasharik.chapter_4_trees_graphs.question_4_1.BinaryNode;
 import ru.pasharik.chapter_4_trees_graphs.question_4_1.GraphUtils;
 import ru.pasharik.chapter_4_trees_graphs.question_4_2.MinimalTree;
 
-import java.util.concurrent.atomic.AtomicBoolean;
-
 public class CheckBalanced {
     // get height of left subtree
+    // if left height < 0
+    //   return -1
     // get height of right subtree
+    // if right height < 0
+    //   return -1
     // if difference in height > 1
-    //    set flag = false
+    //   return -1
     // return max(hLeft, hRight) + 1
-    private int process(BinaryNode n, AtomicBoolean flag) {
+    private int process(BinaryNode n) {
         if (n == null) return 0;
-        int hLeft = process(n.left, flag);
-        int hRight = process(n.right, flag);
-        if (Math.abs(hLeft - hRight) > 1) flag.set(false);
+        int hLeft = process(n.left);
+        if (hLeft < 0) return -1;
+        int hRight = process(n.right);
+        if (hRight < 0) return -1;
+        if (Math.abs(hLeft - hRight) > 1) return -1;
         return Math.max(hLeft, hRight) + 1;
     }
 
+    private boolean isBalanced(BinaryNode n) {
+        return process(n) > 0;
+    }
+
     private void doStart(BinaryNode head) {
-        AtomicBoolean isBalanced = new AtomicBoolean(true);
-        process(head, isBalanced);
-        System.out.println("Balanced: " + isBalanced.get());
+        System.out.println("Balanced: " + isBalanced(head));
     }
 
     public static void main(String[] args) {
